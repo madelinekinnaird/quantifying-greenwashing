38e8e3136fff5fa87bffb35ca1798a3719c27286
Madeline Kinnaird <mk2080@georgetown.edu>
2020-12-05 23:22:04 -0500

    Update for company key

--- data/data_scrapers/esg_scraper/create_corporate_search_list.py
+++ data/data_scrapers/esg_scraper/create_corporate_search_list.py
@@ -1,3 +1,4 @@
+## driver
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,56 +6,53 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-
+## data manipulation
 import pandas as pd
 import numpy as np
 
-## import list of fortune 500 companies
-data = pd.read_csv('../data/fortune_500_companies.csv')
-data[column] = data[column].astype(str)
+
+
+## import company key and set column
+df = pd.read_csv('../../../data/company_key.csv')
+column = 'company'
+companies = df[column]
+
+## url to search on
 url = 'https://www.csrhub.com/'
 
-column = 'NAME'
-df = data
+## start driver
+driver = webdriver.Chrome('C:/Program Files/chromedriver.exe')
 
-## remove rows where there isn't a valid instagram
-df = df[df.INSTAGRAM != 'x']
-
-
-f = open("batch.txt","w+")
+## list to store proper names
+esg_company = []
 
 
-for row in df[column]:
-    ## skip rows with an x
-    if row == 'x':
-        pass
-    else:
-        print(row)
-        ## open chrome driver
-        driver = webdriver.Chrome('C:/Program Files/chromedriver.exe')
 
-        ## go to URL
+for row in companies:
+    ## go to URL
+    driver.get(url)
 
-        driver.get(url)
+    ## click on search feature
+    search_click = driver.find_element_by_xpath('//*[@id="select2-lookup-string-min-container"]/span').click()
+    time.sleep(1)
+    search = driver.find_element_by_xpath('/html/body/span/span/span[1]/input')
+    time.sleep(1)
+    search.send_keys(row)
 
-
-        search_click = driver.find_element_by_xpath('//*[@id="select2-lookup-string-min-container"]/span').click()
-        time.sleep(1)
-        search = driver.find_element_by_xpath('/html/body/span/span/span[1]/input')
-        time.sleep(1)
-        search.send_keys(row)
-
-        ## search for proper name
-        lookup = driver.find_element_by_xpath('//*[@id="select2-lookup-string-min-results"]')
-        time.sleep(2)
-        proper_name = lookup.text
-        proper_name = proper_name.split('\n')[0]
+    ## search for proper name
+    lookup = driver.find_element_by_xpath('//*[@id="select2-lookup-string-min-results"]')
+    time.sleep(2)
+    proper_name = lookup.text
+    proper_name = proper_name.split('\n')[0]
 
 
-        print(proper_name)
+    print(proper_name)
+    esg_company.append(proper_name)
 
-        f.write(proper_name + '\n')
-        driver.close()
-        time.sleep(3)
 
-f.close()
+## input updated list into df column
+df['esg_company'] = esg_company
+
+## update current csv
+path = '../../../data'
+df.to_csv(os.path.join(path,r'company_key.csv'))
